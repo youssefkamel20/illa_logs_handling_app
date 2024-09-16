dd308b0c60ba032490b03271062a5e661a53f5c1
youssefkamel20 <youssefkamel257@gmail.com>
2024-09-16 12:05:20 +0300

    set window min size

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,33 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:illa_logs_app/firebase_options.dart';
 import 'package:illa_logs_app/layout/user_layout.dart';
 import 'package:illa_logs_app/shared/bloc_observer.dart';
-
-
-const apiKey = "AIzaSyDAoeiLHWK37bXUZ6rq-T33x7Aj0Oke23o";
-const projectId = "illa-logs-dummy";
+import 'package:window_manager/window_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  WindowManager.instance.setMinimumSize(const Size(720, 480));
+  WindowManager.instance.hasShadow();
   Bloc.observer = MyBlocObserver();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Illa logs Handling',
+      themeMode: ThemeMode.dark,
       home: UserLayout(),
     );
   }
